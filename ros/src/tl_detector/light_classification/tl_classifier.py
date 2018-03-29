f5da78b06513387dfa44f8cc7367a702a1dc2b25
Robert Ioffe <robert.ioffe@intel.com>
2018-03-29 07:11:15 -0700

    trying ensemble models without much luck

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -7,12 +7,26 @@ from PIL import Image
 import numpy as np
 
 class TLClassifier(object):
-    def __init__(self, model_path='light_classification/models/ssd_sim_and_real_24_03_2018'):
+    #def __init__(self, model_path='light_classification/models/ssd_sim_and_real_24_03_2018'):
     #def __init__(self, model_path='light_classification/models/frozen_sim_inception13'):
     #def __init__(self, model_path='light_classification/models/frozen_real_inception13'):
-    #def __init__(self, model_path='light_classification/models/frozen_faster_rcnn_sim_v2'):
+    def __init__(self, model_path='light_classification/models/frozen_faster_rcnn_sim_v2'):
     #def __init__(self, model_path='light_classification/models/frozen_faster_rcnn_reallife_v2'):
-        self.detector = TLDetector(model_path)
+        #self.detector = TLDetector(model_path)
+        self.detector1 = TLDetector('light_classification/models/ssd_sim_and_real_24_03_2018')
+        self.detector2 = TLDetector('light_classification/models/frozen_sim_inception13')
+
+    def get_state_string(self, state):
+        if (state == 0):
+            state_s = "RED"
+        elif (state == 1):
+            state_s = "YELLOW"
+        elif (state == 2):
+            state_s = "GREEN"
+        else:
+            state_s = "UNKNOWN"
+
+        return state_s
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -31,12 +45,30 @@ class TLClassifier(object):
                 4 : TrafficLight.UNKNOWN
                 }
 
-        output_dict = self.detector.run_inference_for_single_image(image)
+        output_dict1 = self.detector1.run_inference_for_single_image(image)
+        output_dict2 = self.detector2.run_inference_for_single_image(image)
 
-        tl_class = TrafficLight.UNKNOWN
-        if output_dict['detection_scores'][0] >= 0.4:
-            tl_class = tl_class_index.get(output_dict['detection_classes'][0],
+        tl_class1 = TrafficLight.UNKNOWN
+        if output_dict1['detection_scores'][0] >= 0.4:
+            tl_class1 = tl_class_index.get(output_dict1['detection_classes'][0],
                     TrafficLight.UNKNOWN)
+
+        tl_class2 = TrafficLight.UNKNOWN
+        if output_dict2['detection_scores'][0] >= 0.4:
+            tl_class2 = tl_class_index.get(output_dict2['detection_classes'][0],
+                    TrafficLight.UNKNOWN)
+
+        
+        rospy.loginfo('Yury says  : {} with {}'.format(self.get_state_string(tl_class1), output_dict1['detection_scores'][0]))
+        rospy.loginfo('Duksan says: {} with {}'.format(self.get_state_string(tl_class2), output_dict2['detection_scores'][0]))
+        
+        if (tl_class1 == tl_class2):
+            tl_class = tl_class1
+        else:
+            if (output_dict1['detection_scores'][0] > output_dict2['detection_scores'][0]):
+                tl_class = tl_class1
+            else: 
+                tl_class = tl_class2
 
         return tl_class
 
